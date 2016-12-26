50456b6e63405a84ab312b38a858ac824bd94fff
Jonathan Cohlmeyer <jcohlmeyer@jrc9.ca>
2016-12-26 23:18:59 +0000

    Move labels and instructions into resources to support easy translations

---
+++ resources/lang/en/settings.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'tracking_id' => 'Google Analytics Tracking ID',
+    'tracking_id_instruct' => "Your Google Analytics Tracking ID. You can obtain this value from Google Analytics. It starts with 'UA-'",
+    'async' => 'Async Tracking Code',
+    'async_instruct' => 'Use the Async tracking code for Google Analytics. This can result in a small performance boost for modern browsers. You should only enable this if most users are using IE 9+. More Info: https://developers.google.com/analytics/devguides/collection/analyticsjs/',
+    'track_uid' => 'Track UID',
+    'track_uid_instruct' => 'Track users by their unique Statamic ID (when signed in). Note: you must enable User ID tracking on your Google Analytics property for this setting to have any effect.'
+];
