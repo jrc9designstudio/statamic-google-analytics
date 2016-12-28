9f1b19a856e081be5845b8b2dbc04348cad537a6
Jonathan Cohlmeyer <jcohlmeyer@jrc9.ca>
2016-12-28 01:48:44 +0000

    Only add tracking code if tracking_id is set

--- GaTags.php
+++ GaTags.php
@@ -14,66 +14,70 @@ class GaTags extends Tags
      */
     public function index()
     {
-        $tracking_id = $this->getConfig('tracking_id', 'UA-');
-        $display_features = $this->getConfig('display_features', false);
-        $link_id = $this->getConfig('link_id', false);
-        $async = $this->getConfig('async', false);
-        $track_uid = $this->getConfig('track_uid', false);
-        $beacon = $this->getConfig('beacon', false);
-
-        if ($async)
-        {
-           // Async tracking
-           $tracking_code = "<script>
-               window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;";
-        } else {
-            // Non async tracking
-            $tracking_code = "<script>
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');";
-        }
+        $tracking_id = str_replace(' ', '', $this->getConfig('tracking_id', ''), $value);
         
-        $tracking_code .= "ga('create', '" . $tracking_id . "', 'auto');";
-        
-        if ($display_features)
-		{
-			$tracking_code .= "ga('require', 'displayfeatures');";
-		}
-        
-        if ($link_id)
-        {
-        	// Use Enhanced link attribution if enabled
-        	$tracking_code .= "ga('require', 'linkid');";
-        }
-        
-        if ($beacon)
-		{
-            // Set tracking to beacon in browsers that support it
-            $tracking_code .= "ga('set', 'transport', 'beacon');";
-        }
-        
-        $tracking_code .= "ga('send', 'pageview');";
+        if (!empty($tracking_id))
+        {	
+            $display_features = $this->getConfig('display_features', false);
+	        $link_id = $this->getConfig('link_id', false);
+	        $async = $this->getConfig('async', false);
+	        $track_uid = $this->getConfig('track_uid', false);
+	        $beacon = $this->getConfig('beacon', false);
 
-        if ($track_uid)
-        {
-        	$user = User::getCurrent();
-        	if ($user)
-        	{
-        	    $user_id = $user->id();
-                $tracking_code .= "ga('set', 'userId', " . $user_id  . ");";
-            }
-        }
+	        if ($async)
+	        {
+	           // Async tracking
+	           $tracking_code = "<script>
+	               window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;";
+	        } else {
+	            // Non async tracking
+	            $tracking_code = "<script>
+	                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');";
+	        }
+        
+	        $tracking_code .= "ga('create', '" . $tracking_id . "', 'auto');";
+        
+	        if ($display_features)
+			{
+				$tracking_code .= "ga('require', 'displayfeatures');";
+			}
+        
+	        if ($link_id)
+	        {
+	        	// Use Enhanced link attribution if enabled
+	        	$tracking_code .= "ga('require', 'linkid');";
+	        }
+        
+	        if ($beacon)
+			{
+	            // Set tracking to beacon in browsers that support it
+	            $tracking_code .= "ga('set', 'transport', 'beacon');";
+	        }
+        
+	        $tracking_code .= "ga('send', 'pageview');";
 
-	    $tracking_code .= "</script>";
+	        if ($track_uid)
+	        {
+	        	$user = User::getCurrent();
+	        	if ($user)
+	        	{
+	        	    $user_id = $user->id();
+	                $tracking_code .= "ga('set', 'userId', " . $user_id  . ");";
+	            }
+	        }
 
-	    if ($async)
-        {
-            // Async end tracking
-            $tracking_code .= "<script async src='https://www.google-analytics.com/analytics.js'></script>";
-        }
+		    $tracking_code .= "</script>";
 
-        return $tracking_code;
+		    if ($async)
+	        {
+	            // Async end tracking
+	            $tracking_code .= "<script async src='https://www.google-analytics.com/analytics.js'></script>";
+	        }
+
+	        return $tracking_code;
+	    } // End check for tracking code
     }
 }
