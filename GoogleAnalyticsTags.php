75b9e27dec1d2f1863e6b9d225213d2b59144756
Jonathan Cohlmeyer <jcohlmeyer@jrc9.ca>
2016-12-30 23:20:44 +0000

    Move tracking code to view blade template to keep things clean, for easier updating.

--- GoogleAnalyticsTags.php
+++ GoogleAnalyticsTags.php
@@ -19,8 +19,8 @@ class GoogleAnalyticsTags extends Tags
         if (!empty($tracking_id))
         {	
             $display_features = $this->getConfig('display_features', false);
+            $async = $this->getConfig('async', false);
 	        $link_id = $this->getConfig('link_id', false);
-	        $async = $this->getConfig('async', false);
 	        $beacon = $this->getConfig('beacon', false);
 	        $track_uid = $this->getConfig('track_uid', false);
 	        $ignore_admins = $this->getConfig('ignore_admins', false);
@@ -28,62 +28,16 @@ class GoogleAnalyticsTags extends Tags
 	        {
 	            $user = User::getCurrent();
 	        }
-
-	        if ($async)
+	        else
 	        {
-	           // Async tracking
-	           $tracking_code = "<script>
-	               window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;";
-	        } else {
-	            // Non async tracking
-	            $tracking_code = "<script>
-	                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');";
-	        }
-        
-            if ($ignore_admins && $user && $user->can('cp:access'))
-            {
-            	$tracking_code .= "window['ga-disable-' . $tracking_id] = true";
-            }
-        
-	        $tracking_code .= "ga('create', '" . $tracking_id . "', 'auto');";
-        
-	        if ($display_features)
-			{
-				$tracking_code .= "ga('require', 'displayfeatures');";
-			}
-        
-	        if ($link_id)
-	        {
-	        	// Use Enhanced link attribution if enabled
-	        	$tracking_code .= "ga('require', 'linkid');";
-	        }
-        
-            if ($beacon)
-			{
-	            // Set tracking to beacon in browsers that support it
-	            $tracking_code .= "ga('set', 'transport', 'beacon');";
-	        }
-            
-	        $tracking_code .= "ga('send', 'pageview');";
-
-	        if ($track_uid && $user)
-	        {
-	        	$user_id = $user->id();
-	            $tracking_code .= "ga('set', 'userId', " . $user_id  . ");";
+	        	$user = false;
 	        }
 
-		    $tracking_code .= "</script>";
-
-		    if ($async)
-	        {
-	            // Async end tracking
-	            $tracking_code .= "<script async src='https://www.google-analytics.com/analytics.js'></script>";
-	        }
-
-	        return $tracking_code;
-	    } // End check for tracking code
+			return $this->view('tracking-code', compact('tracking_id', 'async', 'display_features', 'link_id', 'beacon', 'track_uid', 'ignore_admins', 'user'))->render();
+	    }
+	    else
+	    {
+	    	return '<!-- Google Analytics Tracking code is not setup yet! -->';
+	    }
     }
 }
---
+++ resources/views/tracking-code.blade.php
@@ -0,0 +1,50 @@
+@if ($async)
+	{{-- Async start Google Analytics --}}
+    <script>
+	   window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+@else
+	{{-- Non async start Google Analytics and add remote script to DOM --}}
+	<script>
+	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+@endif
+
+@if ($ignore_admins && $user && $user->can('cp:access'))
+	{{-- Disable tracking if ignore admins is true, user is signed in and can access cp --}}
+	window['ga-disable-' . $tracking_id] = true;
+@endif
+    
+    {{-- Start tacking code --}}    
+	ga('create', '{{ $tracking_id }}', 'auto');
+        
+@if ($display_features)
+	{{-- Use Display Features if enabled --}}
+	ga('require', 'displayfeatures');
+@endif
+        
+@if ($link_id)
+	{{-- Use Enhanced link attribution if enabled --}}
+    ga('require', 'linkid');
+@endif
+        
+@if ($beacon)
+	{{-- Set tracking to beacon in browsers that support it --}}
+	ga('set', 'transport', 'beacon');
+@endif
+
+@if ($track_uid && $user)
+	ga('set', 'userId', '{{ $user->id() }}');
+@endif
+
+	{{-- Send the pageview --}}            
+	ga('send', 'pageview');
+	
+	{{-- Always end the script tag --}}
+	</script>
+	
+@if ($async)
+	{{-- If async we need to include the remote script at the end --}}
+	<script async src='https://www.google-analytics.com/analytics.js'></script>
+@endif
